81c2fffa81b19f855f6a41088079340d2732bea2
Stickbug <supercrafter813+github@gmail.com>
2025-07-23 17:59:24 -0400

    Small Change

---
+++ Logging/CalloutMessages.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Rage;
+
+namespace EverydayCallouts.Logging
+{
+    internal class CalloutWarnMessages
+    {
+        public static void PedDoesNotExist(){ Game.LogTrivial("[EverydayCallouts]: Ped does not exist. Callout will now end."); }
+
+    }
+
+    internal class CalloutInfoMessages
+    {
+        public static void RNUIMenuAdded() { Game.LogTrivial("[EverydayCallouts]: Menu added to RageNativeUI."); }
+
+    }
+}
+        
